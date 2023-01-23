a49a608443becb8932e28226fb8f3c302918d1a1
BeaComp <102777924+BeaComp@users.noreply.github.com>
2023-01-23 20:39:36 -0300

    Going up project

---
+++ server/src/server.ts
@@ -0,0 +1,29 @@
+//BACK-END 
+
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes";
+
+const app = Fastify()
+
+app.register(cors)
+app.register(appRoutes)
+
+
+//Rota para aparecer na tela
+// '/' seria a rota, caso eu mude para /hello
+// Teria que mudar também para acessar
+//Async e Await vão retornar os dados mais rapidos
+// app.get('/', async () => {
+//     const habits = await prisma.habit.findMany()
+
+//     return habits
+// })
+
+// Porta do localhost
+app.listen({
+    port: 3333,
+    host: '0.0.0.0',
+}).then(() => {
+    console.log('HTTP Server running!')
+})
